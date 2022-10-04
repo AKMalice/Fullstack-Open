e553aeac22e73044ec7ba3dbf4475e763a7ea461
Abdul Nayeem Khalid <abdulnayeem2013@gmail.com>
2022-10-04 17:00:46 +0530

    Added Part 3.01 to 3.11

--- Part2/phonebook/src/Servercom.js
+++ Part2/phonebook/src/Servercom.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const URL = 'http://localhost:3001/persons'
+const URL = 'http://localhost:3001/api/persons'
 
 const getPersons = () =>{
     return axios.get(URL)
@@ -10,11 +10,11 @@ const addPerson = (newPerson) =>{
 }
 
 const deletePerson=(id)=>{
-    return axios.delete(`http://localhost:3001/persons/${id}`)
+    return axios.delete(`http://localhost:3001/api/persons/${id}`)
 }
 
 const updatePerson=(id,newPerson)=>{
-    return axios.put(`http://localhost:3001/persons/${id}`,newPerson)
+    return axios.put(`http://localhost:3001/api/persons/${id}`,newPerson)
 }
 
 export default {getPersons,addPerson,deletePerson,updatePerson}
